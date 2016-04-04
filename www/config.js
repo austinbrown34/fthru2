5ff3848a23047793f8c406c463c3ae39e877bf93
Austin Brown <austinbrown34@gmail.com>
2016-04-04 15:31:13 -0600

    minimized js

--- www/config.js
+++ www/config.js
@@ -1,5 +1,1 @@
-// Global Config Vars
-FTHRU_SETTINGS = {
-  API_HOSTNAME: "development.fthru.cogni.design",
-  EDMUNDS_API_KEY: '6gkv78s45kfx5cbgcf4mq5ae'
-}
+FTHRU_SETTINGS={API_HOSTNAME:"development.fthru.cogni.design",EDMUNDS_API_KEY:'6gkv78s45kfx5cbgcf4mq5ae'}
